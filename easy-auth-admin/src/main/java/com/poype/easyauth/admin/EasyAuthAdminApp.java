efd2dd61ffba1c54875e03081efece78567561e2
poype <253186926@qq.com>
2022-07-07 08:10:00 +0800

    init project

---
+++ easy-auth-admin/src/main/java/com/poype/easyauth/admin/EasyAuthAdminApp.java
@@ -0,0 +1,13 @@
+package com.poype.easyauth.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyAuthAdminApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyAuthAdminApp.class, args);
+    }
+
+}
---
+++ easy-auth-admin/src/main/java/com/poype/easyauth/admin/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.poype.easyauth.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
