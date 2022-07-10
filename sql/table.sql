900539abbfec0a51d62f1b37405947e568a1b770
poype <253186926@qq.com>
2022-07-10 11:43:25 +0800

    create table

--- sql/table.sql
+++ sql/table.sql
@@ -9,4 +9,44 @@ CREATE TABLE `identification` (
    PRIMARY KEY (`id`),
    UNIQUE KEY `username_UNIQUE` (`username`),
    UNIQUE KEY `user_id_UNIQUE` (`user_id`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `role` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `status` tinyint NOT NULL,
+    `name` varchar(50) NOT NULL,
+    `description` varchar(512) NOT NULL,
+    `permission_id_list` varchar(1024) NOT NULL,
+    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `user_role_rel` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `status` tinyint NOT NULL,
+    `user_id` char(15) NOT NULL,
+    `role_id` int NOT NULL,
+    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `permission` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `status` tinyint NOT NULL,
+    `name` varchar(50) NOT NULL,
+    `description` varchar(512) NOT NULL,
+    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `user` (
+    `id` char(15) NOT NULL AUTO_INCREMENT,
+    `status` tinyint NOT NULL,
+    `nickname` varchar(50) NOT NULL,
+    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
